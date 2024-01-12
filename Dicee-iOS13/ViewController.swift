2a74eae5afc14e6aa208a13487e1006ae48bb95f
Rob Harting <robharting@hotmail.com>
2024-01-12 16:24:19 +0100

    with randomizer

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // create this via OPTION + CLick and drag to here to reference a UI element (IB stands for Interface Builder).
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+              
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        var diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image = diceArray [Int.random(in: 0...5)]
+        diceImageView2.image = diceArray [Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
