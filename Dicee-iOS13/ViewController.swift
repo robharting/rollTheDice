d7405c835e6c62cb9e7f542d8e4994ca768dbec0
Rob Harting <robharting@hotmail.com>
2024-01-12 16:34:52 +0100

    refactorred to make it cleamer

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,15 +13,14 @@ class ViewController: UIViewController {
     // create this via OPTION + CLick and drag to here to reference a UI element (IB stands for Interface Builder).
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-              
+   
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        var diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        // use the constant let instead of var
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
         
-        diceImageView1.image = diceArray [Int.random(in: 0...5)]
+        // two ways to randomizer
+        diceImageView1.image = diceArray.randomElement()
         diceImageView2.image = diceArray [Int.random(in: 0...5)]
         
     }
